8f6dc3db45a00abfe2110ed6966f461bf534ff74
kesharaJayasinghe <kesharalahiru1@gmail.com>
2020-08-05 13:39:04 +0530

    Add price filter slider

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { Ng5SliderModule } from 'ng5-slider';
 
 
 @NgModule({
@@ -12,7 +13,8 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    Ng5SliderModule,
   ],
   declarations: [HomePage]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Options, LabelType, ChangeContext, PointerType } from "ng5-slider";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
+  minValue: number = 100;
+  maxValue: number = 300;
+  options: Options = {
+    floor: 0,
+    ceil: 1000,
+    step: 10,
+    minRange: 100,
+    maxRange: 600,
+    pushRange: true,
+    draggableRange: true,
+    translate: (value: number): string => {
+      return "$" + value;
+    },
+  };
 
   constructor() {}
-
 }
